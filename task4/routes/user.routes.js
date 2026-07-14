0db4938f152b6a34544e5b8bbb87f1985844a4d3
Hayam Mostafa <hiammostafa545@gmail.com>
2026-07-14 21:05:56 +0300

    /
    Removed commented-out route definitions for user operations.

--- task4/routes/user.routes.js
+++ task4/routes/user.routes.js
@@ -13,22 +13,11 @@ import { authorization } from "../middlewares/authorization.middleware.js";
 
 const router = express.Router();
 
-// Search
 router.get("/search", authentication, searchUser);
-
-// Get All Users
 router.get("/", authentication, getAllUsers);
-
-// Get User By ID
 router.get("/:id", authentication, getUserById);
-
-// Create User (Admin only)
 router.post("/", authentication, authorization("Admin"), addUser);
-
-// Update User
 router.put("/:id", authentication, updateUser);
-
-// Delete User (Admin only)
 router.delete(
   "/:id",
   authentication,
